98a0b6ebb7eaa3e6c0b1df27ddaf9579834c6a1f
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2020-05-07 17:15:35 +0530

    Section 1 LCO Completed

---
+++ helloWorld/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // needed for v-1 of this program
+#include <cstdio> // needed for the v-2 of this program
+using namespace std;
+
+//// program v-1
+
+int main(){
+    cout << "Hello World! \n";
+    return 0;
+}
+
+
+//// program v-2
+
+// int main(){
+//     puts("Click on button");
+//     puts("create a new player");
+//     puts("Add life to player");
+//     return 0;
+// }
